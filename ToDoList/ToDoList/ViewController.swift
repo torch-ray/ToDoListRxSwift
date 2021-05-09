00f11f6fbff444f6b029777cabe12d378ffa8989
Ray suh <readingod@naver.com>
2021-05-09 16:23:38 +0900

    불필요한 주석 삭제 및 사용하지 않는 메서드 주석처리

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  ToDoList
-//
-//  Created by 서우석 on 2021/05/09.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
