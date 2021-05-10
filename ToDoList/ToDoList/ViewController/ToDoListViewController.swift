762f06b0dfde879761674b6f66fd3f14e5881cff
Ray suh <readingod@naver.com>
2021-05-10 10:02:36 +0900

    [Step.5]Feat: ToDoListViewController에서 bind메서드 구현 및 CustomCell 생성

--- ToDoList/ToDoList/ViewController/ToDoListViewController.swift
+++ ToDoList/ToDoList/ViewController/ToDoListViewController.swift
@@ -1,14 +1,27 @@
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class ToDoListViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: ToDoListViewModel!
-    
+    @IBOutlet weak var listCollectionView: UICollectionView!
+    @IBOutlet weak var addListButton: UIBarButtonItem!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.todoList
+            .bind(to: listCollectionView.rx.items(cellIdentifier: "Cell", cellType: ToDoCell.self)) { _, list, cell in
+                cell.configure(title: list.title, content: list.content)
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
