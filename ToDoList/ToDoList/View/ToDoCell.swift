40e60ace580b5a74fa5c9b851ef500d99656e1c9
Ray suh <readingod@naver.com>
2021-05-11 22:26:35 +0900

    [Step.6]Feat: RandomColor 프레임워크 추가 및 프레임워크를 활용한 간단한 뷰디자인

--- ToDoList/ToDoList/View/ToDoCell.swift
+++ ToDoList/ToDoList/View/ToDoCell.swift
@@ -1,12 +1,34 @@
 import UIKit
+import RandomColor
 
 class ToDoCell: UICollectionViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var leadingView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCellElement()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupCellElement()
+    }
     
     func configure(title: String, content: String) {
+        let color = RC.color()
         titleLabel.text = title
+        titleLabel.textColor = color
         contentLabel.text = content
+        leadingView.backgroundColor = color
+    }
+}
+
+private extension ToDoCell {
+    private func setupCellElement() {
+        layer.masksToBounds = true
+        layer.cornerRadius = 5
     }
 }
